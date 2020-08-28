796e71f807814685c70a5fd88b1555dcc8e93654
Nicolas Bayle <nicolas.bayle.it@gmail.com>
2020-08-28 20:58:26 +0000

    20200828

--- userdata/opencart.sh
+++ userdata/opencart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt-get update
+# sudo apt install -y mysql-server
+# sudo apt install -y python3-pymysql
 sudo apt-add-repository -y ppa:ondrej/php
 sudo apt-get update
 sudo apt install -y php7.4
@@ -8,16 +10,16 @@ sudo apt install -y apache2
 sudo apt install -y libapache2-mod-php
 sudo apt install -y libmcrypt-dev
 sudo apt-get -y install unzip
-wget https://github.com/opencart/opencart/releases/download/3.0.3.5/opencart-3.0.3.5.zip
-sudo mkdir /var/www/opencart.avidemo.fr/
-sudo unzip opencart-3.0.3.5.zip -d /var/www/opencart.avidemo.fr/
-sudo mv /var/www/opencart.avidemo.fr/upload/config-dist.php /var/www/opencart.avidemo.fr/upload/config.php
-sudo mv /var/www/opencart.avidemo.fr/upload/.htaccess.txt /var/www/opencart.avidemo.fr/upload/.htaccess
-sudo mv /var/www/opencart.avidemo.fr/upload/admin/config-dist.php /var/www/opencart.avidemo.fr/upload/admin/config.php
-sudo rm -f /var/www/opencart.avidemo.fr/*
-sudo mv -f /var/www/opencart.avidemo.fr/upload/* /var/www/opencart.avidemo.fr/
-sudo mv -f /var/www/opencart.avidemo.fr/upload/.* /var/www/opencart.avidemo.fr/
-sudo rmdir /var/www/opencart.avidemo.fr/upload/
-sudo chmod -R 755 /var/www/opencart.avidemo.fr/
-sudo chown -R www-data:www-data /var/www/opencart.avidemo.fr/
+wget ${opencartDownloadUrl}
+sudo mkdir /var/www/${aviVsOpencart}.${domainName}/
+sudo unzip opencart-3.0.3.5.zip -d /var/www/${aviVsOpencart}.${domainName}/
+sudo mv /var/www/${aviVsOpencart}.${domainName}/upload/config-dist.php /var/www/${aviVsOpencart}.${domainName}/upload/config.php
+sudo mv /var/www/${aviVsOpencart}.${domainName}/upload/.htaccess.txt /var/www/${aviVsOpencart}.${domainName}/upload/.htaccess
+sudo mv /var/www/${aviVsOpencart}.${domainName}/upload/admin/config-dist.php /var/www/${aviVsOpencart}.${domainName}/upload/admin/config.php
+sudo rm -f /var/www/${aviVsOpencart}.${domainName}/*
+sudo mv -f /var/www/${aviVsOpencart}.${domainName}/upload/* /var/www/${aviVsOpencart}.${domainName}/
+sudo mv -f /var/www/${aviVsOpencart}.${domainName}/upload/.* /var/www/${aviVsOpencart}.${domainName}/
+sudo rmdir /var/www/${aviVsOpencart}.${domainName}/upload/
+sudo chmod -R 755 /var/www/${aviVsOpencart}.${domainName}/
+sudo chown -R www-data:www-data /var/www/${aviVsOpencart}.${domainName}/
 echo "cloud init done" | tee /tmp/cloudInitDone.log
